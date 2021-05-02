9ca9f9cf59f482ff817a158f80963d176b9be308
andrewshakirov <ewen9936@gmail.com>
2021-05-02 20:41:49 +0300

    removed select from with_category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.with_category(category_name)
-    select(:title).joins('JOIN categories ON tests.category_id = categories.id').where(
+    joins('JOIN categories ON tests.category_id = categories.id').where(
       categories: { title: category_name }
     ).order(title: :desc).map(&:title)
   end
