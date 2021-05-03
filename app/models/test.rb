5ebff5670a890f5dec8f0716d87d32d850c258e6
andrewshakirov <ewen9936@gmail.com>
2021-05-03 15:14:05 +0700

    changed map to pluck

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
   def self.with_category(category_name)
     joins('JOIN categories ON tests.category_id = categories.id').where(
       categories: { title: category_name }
-    ).order(title: :desc).map(&:title)
+    ).order(title: :desc).pluck(:title)
   end
 end
