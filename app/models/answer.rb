22045ad0bbe0eb1f5ab9b2bba8288b5f9023b030
andrewshakirov <ewen9936@gmail.com>
2021-05-08 00:37:05 +0700

    fixed count validation in answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,11 +4,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answers_count
+  validate :validate_answers_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   def validate_answers_count
-    errors.add(:answers_count) unless (0..3).include?(question.answers.count)
+    errors.add(:answers_count) if question.answers.count >= 4
   end
 end
