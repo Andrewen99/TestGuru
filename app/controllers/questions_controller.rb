e017b59c2cbfd641f5eadd38681d7f6472ff4ee8
andrewshakirov <ewen9936@gmail.com>
2021-05-10 17:22:53 +0300

    corrected mistakes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,40 +1,38 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
-  before_action :find_question, only: %i[show destroy]
-  before_action :find_questions, only: %i[index create]
+  before_action :find_test, except: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def show; end
+  def show
+    @question = Question.find(params[:id])
+  end
 
   def index
+    @questions = @test.questions
     render inline: "<p>Questions for '<%= @test.title %>' test: <%= @questions.pluck(:id, :body) %></p>"
   end
 
   def new; end
 
   def create
-    question = @questions.create(question_params)
-    render plain: question.inspect
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   def destroy
+    @question = Question.find(params[:id])
     @question.destroy
-    redirect_to test_questions_path(@test)
+    redirect_to test_questions_path(@question.test)
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
-  end
-
-  def find_question
-    @question = @test.questions.find(params[:id])
-  end
-
-  def find_questions
-    @questions = @test.questions
   end
 
   def question_params
