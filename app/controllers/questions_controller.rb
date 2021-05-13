a299a4178eeefe0651ad1dc157b4fac9efa6c12e
andrewshakirov <ewen9936@gmail.com>
2021-05-13 18:54:50 +0300

    task6 fixed mistakes2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,15 @@
 class QuestionsController < ApplicationController
   before_action :find_test, except: %i[show destroy]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  def show
-    @question = Question.find(params[:id])
-  end
 
   def index
     @questions = @test.questions
     render inline: "<p>Questions for '<%= @test.title %>' test: <%= @questions.pluck(:id, :body) %></p>"
   end
+
+  def show; end
 
   def new; end
 
@@ -24,7 +23,6 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
     redirect_to test_questions_path(@question.test)
   end
@@ -33,6 +31,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
